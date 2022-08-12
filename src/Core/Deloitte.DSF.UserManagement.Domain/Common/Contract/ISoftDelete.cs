8f4aee64158ec8317f385d7b6c2916ce7393b32c
Sudarshan Jhawar <sujhawar@deloitte.com>
2022-08-12 14:22:23 +0530

    added user entity + domain done

--- src/Core/Deloitte.DSF.UserManagement.Domain/Common/Contract/ISoftDelete.cs
+++ src/Core/Deloitte.DSF.UserManagement.Domain/Common/Contract/ISoftDelete.cs
@@ -4,4 +4,5 @@ public interface ISoftDelete
 {
     DateTimeOffset? DeletedOn { get; set; }
     Guid? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
 }
---
+++ src/Core/Deloitte.DSF.UserManagement.Domain/User/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿public class ApplicationUser : AuditableEntity, IAggregateRoot, ISoftDelete
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EMail { get; set; }
+    public DateTimeOffset? DeletedOn { get; set; }
+    public Guid? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
+
+    public ApplicationUser(string firstName, string lastName, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        EMail = email;
+    }
+}
